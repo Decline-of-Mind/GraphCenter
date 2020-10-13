890c175d88b31986dd9030806360435fe9188136
Luc Gotink <L.h.gotink@gmail.com>
2020-10-13 13:52:30 +0000

    changed variables for AWS

--- graphcenter/settings.py
+++ graphcenter/settings.py
@@ -159,7 +159,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 if 'USE_AWS' in os.environ:
     # Configuration for S3 Bucket
     AWS_STORAGE_BUCKET_NAME = 'graphcenter'
-    AWS_S3_REGION_NAME = 'Global'
+    AWS_S3_REGION_NAME = 'EU(Paris)'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME }.s3.amazonaws.com'
