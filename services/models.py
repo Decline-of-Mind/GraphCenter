69f0e8b24952d8a9eacc9af7deda6032fe334722
Luc Gotink <L.h.gotink@gmail.com>
2020-10-27 21:29:51 +0000

    added services app and added categories and services to the database

--- services/models.py
+++ services/models.py
@@ -3,16 +3,21 @@ from django.db import models
 # Create your models here.
 
 class Category(models.Model):
-        
+
     class Meta:
         verbose_name_plural = 'Categories'
 
     name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
         return self.name
 
-class Services(models.Model):
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Service(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     name = models.CharField(max_length=254)
     description = models.TextField()
--- services/views.py
+++ services/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def services(request):
+    """ view for the services page """
+    return render(request, 'services/services.html')
