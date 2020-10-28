bfc1ef9bc2b6ebf3f5b4177bf9dae3e0120e49dc
Luc Gotink <L.h.gotink@gmail.com>
2020-10-28 12:03:35 +0000

    customized admin layout

--- services/admin.py
+++ services/admin.py
@@ -2,6 +2,27 @@ from django.contrib import admin
 from .models import Service, Category
 
 # Register your models here.
-admin.site.register(Category)
-admin.site.register(Service)
+
+
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'price',
+        'image',
+    )
+
+    ordering = ('category',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'friendly_name'
+    )
+
+
+admin.site.register(Category, CategoryAdmin)
+
+admin.site.register(Service, ServiceAdmin)
 
