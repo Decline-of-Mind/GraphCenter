c673d928cdb98e3d2548d6edf47791b33a2a7dcd
Luc Gotink <L.h.gotink@gmail.com>
2020-11-04 14:51:09 +0000

    added nav-links, sorting of categories and css for services page

--- services/views.py
+++ services/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Service
+from .models import Service, Category
 
 # Create your views here.
 
@@ -8,9 +8,17 @@ def services(request):
     """ view for the services page """
 
     services = Service.objects.all()
+    category = None
+
+    if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            services = services.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
 
     context = {
         'services': services,
+        'current_categories': categories,
     }
 
     return render(request, 'services/services.html', context)
