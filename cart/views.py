928a95fe03d9bbc7a0bc6ff0ce644add3dcc118b
Luc Gotink <L.h.gotink@gmail.com>
2020-11-15 20:34:00 +0000

    added basic add_to_cart functionality

--- cart/views.py
+++ cart/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.exceptions import ObjectDoesNotExist
 
+from services.models import Service
 
 # Create your views here.
 
@@ -15,19 +16,27 @@ def view_cart(request):
 
 
 def add_to_cart(request, item_id):
-    quantity = 0
+    """ Add a product to the shopping cart"""
+
+    service = get_object_or_404(Service, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
 
+    # Checks if the item is already in the bag, returns error if it does.
     if item_id in list(cart.keys()):
         messages.error(
             request, 'Only one of the same service allowed')
         return redirect(redirect_url)
     else:
         cart[item_id] = quantity
+        messages.success(request, f'Added {service.name} to your shopping bag')
+        print(f'Added {service.name} to your shopping cart')
 
     request.session['cart'] = cart
-    return redirect('cart:view_cart')
+    print(request.session['cart'])
+    return redirect(redirect_url)
 
 
     
